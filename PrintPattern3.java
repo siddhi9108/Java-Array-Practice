3be866cf1e18b46857f5ee6457f87e6b97158fd9
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-11-30 11:45:54 +0530

    Add files via upload

---
+++ PrintPattern3.java
@@ -0,0 +1,15 @@
+package com.siddhi.arrays;
+
+public class PrintPattern3 {
+	// Decreasing Triangle
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j <= n; j++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
