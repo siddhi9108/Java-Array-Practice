266faca8a372dab7eb2141e07f2b605d29d2547e
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-12-19 11:47:58 +0530

    Add files via upload

---
+++ SumOfArrays.java
@@ -0,0 +1,28 @@
+package com.siddhi.arrays;
+
+import java.util.Scanner;
+
+public class SumOfArrays {
+
+	public static void main(String[] args) {
+		Scanner ob = new Scanner(System.in);
+		System.out.println("Enter x: ");
+		int x = ob.nextInt();
+		System.out.println("Enter your array: ");
+		int[] arr = new int[x];
+		for (int i = 0; i < x; i++) {
+			arr[i] = ob.nextInt();
+		}
+		System.out.println("Your Array: ");
+		for (int i = 0; i < x; i++) {
+			System.out.print(arr[i] + ",");
+		}
+		int sum = 0;
+		for (int i : arr) {
+			sum += i;
+		}
+		System.out.println();
+		System.out.println("Your sum: " + sum);
+	}
+
+}
